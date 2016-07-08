00275aa1f26e0eab0fcca726efbcd0e1cc05d2d7
Tony Lawrence <code@tonylawrence.com>
2016-07-08 12:04:04 +0100

    create new account

---
+++ account.rb
@@ -0,0 +1,16 @@
+dep 'create user account' do
+  met? {
+    shell "dscl -q . -read /Users/tony RealName 2> /dev/null"
+  }
+  meet {
+    log "Account creation requires superuser priviledges"
+    sudo "dscl . create /Users/tony"
+    sudo "dscl . create /Users/tony UserShell /bin/bash"
+    sudo "dscl . create /Users/tony RealName 'Tony Lawrence'"
+    sudo "dscl . create /Users/tony UniqueID 555"
+    sudo "dscl . create /Users/tony PrimaryGroupID 1000"
+    sudo "dscl . create /Users/tony NFSHomeDirectory /Local/Users/tony"
+    sudo "dscl . passwd /Users/tony password"
+    sudo "dscl . append /Groups/brew GroupMembership tony"
+  }
+end
